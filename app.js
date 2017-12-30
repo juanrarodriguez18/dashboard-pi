84641f581138945265acbf00e466b23d3a02ef89
juanrarodriguez18 <dragondr3w@gmail.com>
2017-12-30 14:13:42 +0100

    Añadida página de login de ejemplo

--- app.js
+++ app.js
@@ -1,9 +1,24 @@
-var express = require('express');
-var app = express();
+const pug = require('pug');
+const path = require('path');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+// Set views path
+app.set('views', path.join(__dirname, 'views'));
+// Set public path
+app.use(express.static(path.join(__dirname, 'views')));
+
+//Pug pages rendering
+app.set("view engine", "pug");
 
 app.get('/', function (req, res) {
   res.send('Hello World!');
 });
+
+app.get('/login', function (req, res) {
+    res.render("login");
+  });
 
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!');
